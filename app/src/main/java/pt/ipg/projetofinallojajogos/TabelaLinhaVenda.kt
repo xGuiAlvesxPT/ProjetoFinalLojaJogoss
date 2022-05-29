bdd8450d55d3d99f2762e8a99bfd16919af4b0f3
xGuiAlvesxPT <101353229+xGuiAlvesxPT@users.noreply.github.com>
2022-05-29 21:47:21 +0100

    Criação da Class TabelaLinhaVenda!

---
+++ app/src/main/java/pt/ipg/projetofinallojajogos/TabelaLinhaVenda.kt
@@ -0,0 +1,20 @@
+package pt.ipg.projetofinallojajogos
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaLinhaVenda(val db: SQLiteDatabase?) {
+
+    fun cria (){
+
+        db?.execSQL("CREATE TABLE $NOME (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $QUANTIDADE INTEGER NOT NULL, $PERCENTAGEM_IVA INTEGER(9,2) NOT NULL,$CAMPO_FK_JOGOS INTEGER NOT NULL,$CAMPO_FK_VENDA INTEGER NOT NULL,FOREIGN KEY $CAMPO_FK_JOGOS) REFERENCES ${TabelaJogos.NOME} (${BaseColumns._ID}) ON DELETE RESTRICT,FOREIGN KEY($CAMPO_FK_VENDA) REFERENCES ${TabelaVendas.NOME} (${BaseColumns._ID}) ON DELETE RESTRICT) ")
+    }
+
+    companion object{
+        const val NOME = "Linha de Venda "
+        const val QUANTIDADE = "Quantidade"
+        const val PERCENTAGEM_IVA = "Percentagem_iva"
+        const val CAMPO_FK_VENDA = "idVenda"
+        const val CAMPO_FK_JOGOS = "idJogo"
+    }
+
+}
