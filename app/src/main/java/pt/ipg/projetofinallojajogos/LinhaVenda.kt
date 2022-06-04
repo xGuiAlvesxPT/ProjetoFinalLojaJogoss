7cf7945418db675df2a51dcd00b2287ef93a26ba
xGuiAlvesxPT <101353229+xGuiAlvesxPT@users.noreply.github.com>
2022-06-04 22:18:49 +0100

    Alteração de tipos de dados na tabela da Linha de Vendas!

--- app/src/main/java/pt/ipg/projetofinallojajogos/LinhaVenda.kt
+++ app/src/main/java/pt/ipg/projetofinallojajogos/LinhaVenda.kt
@@ -5,7 +5,6 @@ import java.util.*
 class LinhaVenda(
     var quantidade: Int,
     var preco: Float,
-    var percentagemIva: Float,
     var idjogo: Long,
     var idvenda: Long,
     val id: Long = -1
@@ -17,7 +16,6 @@ class LinhaVenda(
         val valores = ContentValues()
         valores.put(TabelaLinhaVenda.QUANTIDADE,quantidade)
         valores.put(TabelaLinhaVenda.PRECO,preco)
-        valores.put(TabelaLinhaVenda.PERCENTAGEM_IVA,percentagemIva)
         valores.put(TabelaLinhaVenda.CAMPO_FK_JOGOS,idjogo)
         valores.put(TabelaLinhaVenda.CAMPO_FK_VENDA,idvenda)
         return valores
