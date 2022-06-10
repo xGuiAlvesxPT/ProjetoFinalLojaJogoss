e90a5e79391c7c41b75214345f8d21a9c75da7ad
xGuiAlvesxPT <101353229+xGuiAlvesxPT@users.noreply.github.com>
2022-06-10 18:38:00 +0100

    Criação de cursor na CLASS cliente para auxiliar as querys!

--- app/src/main/java/pt/ipg/projetofinallojajogos/Cliente.kt
+++ app/src/main/java/pt/ipg/projetofinallojajogos/Cliente.kt
@@ -1,12 +1,11 @@
 package pt.ipg.projetofinallojajogos
 import android.content.ContentValues
-import java.text.DateFormat
-import java.util.*
-import kotlin.time.Duration.Companion.milliseconds
+import android.database.Cursor
+import android.provider.BaseColumns
 
 data class Cliente(
     var nome: String,
-    var sexo: Char,
+    var sexo: String,
     var nif: String,
     var contacto: String,
     var data_de_nascimento: String,
@@ -17,10 +16,30 @@ data class Cliente(
 
         val valores = ContentValues()
         valores.put(TabelaClientes.NOME_CLIENTE,nome)
-        valores.put(TabelaClientes.SEXO,sexo.toString())
+        valores.put(TabelaClientes.SEXO,sexo)
         valores.put(TabelaClientes.NIF_CLIENTE,nif)
         valores.put(TabelaClientes.CONTACTO,contacto)
         valores.put(TabelaClientes.DATA_DE_NASCIMENTO,data_de_nascimento)
         return valores
+    }
+
+    companion object {
+        fun fromCursor(cursor: Cursor): Cliente {
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posNome = cursor.getColumnIndex(TabelaClientes.NOME_CLIENTE)
+            val posSexo = cursor.getColumnIndex(TabelaClientes.SEXO)
+            val posNif = cursor.getColumnIndex(TabelaClientes.NIF_CLIENTE)
+            val posContacto = cursor.getColumnIndex(TabelaClientes.CONTACTO)
+            val posDataNascimento = cursor.getColumnIndex(TabelaClientes.DATA_DE_NASCIMENTO)
+
+            val id = cursor.getLong(posId)
+            val nome = cursor.getString(posNome)
+            val sexo = cursor.getString(posSexo)
+            val nif = cursor.getString(posNif)
+            val contacto = cursor.getString(posContacto)
+            val dataNascimento = cursor.getString(posDataNascimento)
+
+            return Cliente(nome,sexo,nif,contacto,dataNascimento, id)
+        }
     }
 }
