866b523ee4fb7da3b28e2241bfe807d2368badab
xGuiAlvesxPT <101353229+xGuiAlvesxPT@users.noreply.github.com>
2022-06-15 19:47:15 +0100

    Revert "Teste"
    This reverts commit 552cebcd6ccdfee5c9295c72bd88dfcc1c71c0ec.

--- app/src/main/java/pt/ipg/projetofinallojajogos/ContentProviderLojaJogos.kt
+++ app/src/main/java/pt/ipg/projetofinallojajogos/ContentProviderLojaJogos.kt
@@ -265,7 +265,6 @@ class ContentProviderLojaJogos : ContentProvider() {
             uriMatcher.addURI(AUTHORITY, "${TabelaVendas.NOME}/#", URI_VENDA_ESPECIFICA)
 
             return uriMatcher
-            //ola
         }
     }
 }
